4d4e90974d9bacf22b22194b48874ac41ce8c76c
thenaijagamer <sabdullahialaba50@gmail.com>
2024-01-16 18:25:09 +0100

    Add the json data of words

--- js/script.js
+++ js/script.js
@@ -1,2 +1,32 @@
-// Add some interactivity to the website
-console.log("Hello, world!");
+// Fetch JSON words data
+let words;
+fetch("./words.json")
+  .then((response) => response.json())
+  .then((data) => {
+    words = data.words;
+    console.log(words);
+  })
+  .catch((error) => console.error("Error fetching JSON:", error));
+
+// Get all input elements with the class 'single-char-input'
+const inputBoxes = document.querySelectorAll(".single-char-input");
+
+// Add an event listener to each input box
+inputBoxes.forEach((input, index) => {
+  input.addEventListener("input", (event) => {
+    const currentInput = event.target;
+    const nextInput = inputBoxes[index + 1];
+
+    // If the input has a value, move focus to the next input
+    if (currentInput.value && nextInput) {
+      nextInput.focus();
+    }
+  });
+
+  // Add an event listener to move focus back when the input is cleared
+  input.addEventListener("keydown", (event) => {
+    if (event.key === "Backspace" && !input.value && index > 0) {
+      inputBoxes[index - 1].focus();
+    }
+  });
+});
